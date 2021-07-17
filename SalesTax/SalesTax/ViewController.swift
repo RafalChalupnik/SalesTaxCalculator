ab648bdf450331dfcfaa048343613c6c30260ecc
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2021-07-17 19:16:55 +0200

    Capturing values from TextFields

--- SalesTax/SalesTax/ViewController.swift
+++ SalesTax/SalesTax/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var beforeTaxPrice : Float = 0
+    var salesTaxRate : Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func beforeTaxPriceChanged(_ sender: UITextField) {
+        
+        if let value = Float(sender.text!) {
+            beforeTaxPrice = value
+        } else {
+            beforeTaxPrice = 0
+        }
+    }
+    
+    @IBAction func salesTaxRateChanged(_ sender: UITextField) {
+        
+        if let value = Float(sender.text!) {
+            salesTaxRate = value
+        } else {
+            salesTaxRate = 0
+        }
+    }
 }
 
