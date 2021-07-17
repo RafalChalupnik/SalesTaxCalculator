de47aa11abb69b7142adc2551ccce3c3a05a2db5
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2021-07-17 19:25:48 +0200

    Created SwiftUI project

---
+++ SalesTax-SwiftUI/SalesTax-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SalesTax-SwiftUI
+//
+//  Created by Rafał Chałupnik on 17/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
